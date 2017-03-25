499732cd6b03acfc8ade2c211634735b05236879
David Dobrinskiy <david@dobrinskiy.com>
2017-03-25 21:28:14 +0300

    ex1

---
+++ ex1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+    char *test = malloc(100);
+
+    // this is also a comment
+    printf("you are %d miles away %s.\n", distance, test);
+
+    return 0;
+
+}
+
